755f579d87443080fb5d0850592ceaccefe0e7c1
Brandon Wetzel <mauzybwy@gmail.com>
2025-11-24 13:24:19 -0800

    Maybe use timestamptz by default

--- config/test.exs
+++ config/test.exs
@@ -14,7 +14,8 @@ config :poof, Poof.Repo,
   hostname: "localhost",
   database: "poof_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: System.schedulers_online() * 2
+  pool_size: System.schedulers_online() * 2,
+  migration_timestamps: [type: :timestamptz]
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
